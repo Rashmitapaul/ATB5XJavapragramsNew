c5c5abdfafbb0adffe0f7f4149b0655cef202e54
Rashmita_Paul <rashmitapaul2@gmail.com>
2023-12-18 20:02:09 +0530

    I am pushing Lab002.java and Lab003.java

--- src/basicdec09/Lab002.java
+++ src/basicdec09/Lab002.java
@@ -1,4 +1,4 @@
-package src;
+package src.basicdec09;
 
 public class Lab002 {
 
---
+++ src/basicdec09/Lab003.java
@@ -0,0 +1,24 @@
+package src.basicdec09;
+
+public class Lab003 {
+
+/*
+* Author - Rashmita
+* Program - Showcase difference between println & print
+*
+* */
+
+    public static void main(String[] args) {
+       // System.out.print("Hello world");
+        //System.out.println("Hello world");
+        //System.out.println("Rashmita");
+
+        int age=34;
+       // System.out.println("Your Age is " + age);
+        System.out.printf("Your Age is %d", age);
+        // Here %d is a any integer type - byte, short, int or long
+        //%s - string
+
+       //System.out.printf("Your Age is " + age);
+    }
+}
