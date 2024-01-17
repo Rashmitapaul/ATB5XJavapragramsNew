86ca1702b7cb6e7dc9e1ffb29268c1a01ddc6aca
Rashmita_Paul <rashmitapaul2@gmail.com>
2024-01-17 12:50:00 +0530

    I am pushing code.

---
+++ src/basic_oops_dec30/Lab100.java
@@ -0,0 +1,18 @@
+package src.basic_oops_dec30;
+
+public class Lab100 {
+    public static void main(String[] args) {
+      // Functions
+
+     int c = rashmi_sum(3,4);
+        System.out.println(c);
+
+
+    }
+
+    static int rashmi_sum(int input_1 , int input_2) {
+         return input_1 + input_2;
+     }
+
+
+}
